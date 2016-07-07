95d4dcb14e0134edbb2c6210293edf5af57fd23f
chebao <chebao@chebaodeimac.local>
2016-07-07 14:17:25 +0800

    Initial Commit

---
+++ RunDogOC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunDogOC
+//
+//  Created by chebao on 16/7/7.
+//  Copyright © 2016年 chebao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RunDogOC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RunDogOC
+//
+//  Created by chebao on 16/7/7.
+//  Copyright © 2016年 chebao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
